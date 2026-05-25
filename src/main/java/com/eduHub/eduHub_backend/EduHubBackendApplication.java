90a77e79c3c3a0f29100de084c2795d1ce7ed1b5
yaazhvendhan <yaazhvendhanms@gmail.com>
2026-05-25 10:07:18 +0530

    first commit

---
+++ src/main/java/com/eduHub/eduHub_backend/EduHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eduHub.eduHub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduHubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduHub/eduHub_backend/EduHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduHub.eduHub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
