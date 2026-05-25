837f7ef3293a329fac22026dbf290fc63fdbbaf7
yaazhvendhan <yaazhvendhanms@gmail.com>
2026-05-25 15:43:50 +0530

    feat: create a new method to print the environment variables

--- src/main/java/com/eduHub/eduHub_backend/controller/HellowWorldController.java
+++ src/main/java/com/eduHub/eduHub_backend/controller/HellowWorldController.java
@@ -1,13 +1,32 @@
 package com.eduHub.eduHub_backend.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HellowWorldController {
 
+    private final Environment environment;
+    @Value("${spring.application.name}")
+    private String appName;
+
+    public HellowWorldController(Environment environment){
+        this.environment = environment;
+    }
+
+    //to print hello world message
     @GetMapping("hello-world")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    //to print the env variables
+    @GetMapping("/env")
+    public String getEnvironmentVariables(){
+        String port = environment.getProperty("server.port");
+        // int port = context.getWebServer().getPort();  --> this returns the actual running port
+        return "App Name : " + appName + ", Port : "+ port;
     }
 }
