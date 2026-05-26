c179f916eb53d7be0deaea265a0c79e64aa2905d
yaazhvendhan <yaazhvendhanms@gmail.com>
2026-05-26 18:20:00 +0530

    feat: add student endpoint using query parameters

--- src/main/java/com/eduHub/eduHub_backend/controller/StudentController.java
+++ src/main/java/com/eduHub/eduHub_backend/controller/StudentController.java
@@ -1,16 +1,24 @@
 package com.eduHub.eduHub_backend.controller;
 
 import com.eduHub.eduHub_backend.component.StudentService;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class StudentController {
+
+    private final InitializingBean classPathFileSystemWatcher;
+
+    public StudentController(InitializingBean classPathFileSystemWatcher) {
+        this.classPathFileSystemWatcher = classPathFileSystemWatcher;
+    }
 
     // Returns one Student
     @GetMapping("student")
@@ -31,7 +39,7 @@ public class StudentController {
         return new ResponseEntity<>(studentServiceList,HttpStatus.OK);
     }
 
-    // Get Student details with https://localhost:8080/id/firstName/lastName
+    // Get Student details using https://localhost:8080/id/firstName/lastName
     @GetMapping("{id}/{firstName}/{lastName}")
     public ResponseEntity<StudentService> studentServicePathVariable(@PathVariable("id") int studentId,
                                              @PathVariable("firstName") String firstName,
@@ -39,5 +47,15 @@ public class StudentController {
     {
         StudentService studentService2 = new StudentService(studentId, firstName, lastName);
         return new ResponseEntity<>(studentService2,HttpStatus.OK);
+    }
+
+    // Get Student details using http://localhost:8080/query?id=studentId&firstName=firstName&lastName=lastName
+    @GetMapping("query")
+    public ResponseEntity<StudentService> studentServiceResponseVariable(@RequestParam int id,
+                                                                         @RequestParam String firstName,
+                                                                         @RequestParam String lastName)
+    {
+        StudentService studentService3 = new StudentService(id, firstName,lastName);
+        return ResponseEntity.ok(studentService3);
     }
 }
