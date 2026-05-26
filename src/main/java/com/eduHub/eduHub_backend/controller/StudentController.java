59c14a39d0c7c66b08b9ad48ad4869c29ae88aba
yaazhvendhan <yaazhvendhanms@gmail.com>
2026-05-26 17:37:16 +0530

    update: update the StudentController with get request returning multiple values

--- src/main/java/com/eduHub/eduHub_backend/controller/StudentController.java
+++ src/main/java/com/eduHub/eduHub_backend/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.eduHub.eduHub_backend.controller;
 
 import com.eduHub.eduHub_backend.component.StudentService;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -13,5 +17,16 @@ public class StudentController {
     public ResponseEntity<StudentService> getStudent(){
         StudentService studentService = new StudentService(1,"Yaazh","M");
         return new ResponseEntity<>(studentService, HttpStatus.OK);
+    }
+
+    @GetMapping("students")
+    public ResponseEntity<List<StudentService> >getStudents(){
+        List<StudentService> studentServiceList = new ArrayList<>();
+        studentServiceList.add(new StudentService(1,"Yaazh","M"));
+        studentServiceList.add(new StudentService(2,"Saturn","Ring"));
+        studentServiceList.add(new StudentService(3,"String","Ray"));
+        studentServiceList.add(new StudentService(4,"John","Snow"));
+
+        return new ResponseEntity<>(studentServiceList,HttpStatus.OK);
     }
 }
