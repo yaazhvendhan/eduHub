75b1a94199b8d6fe753a9457b96048cee9ff6982
yaazhvendhan <yaazhvendhanms@gmail.com>
2026-05-27 09:08:09 +0530

    feat: create a new CoursesController and implement REST API

---
+++ src/main/java/com/eduHub/eduHub_backend/controller/CoursesController.java
@@ -0,0 +1,77 @@
+package com.eduHub.eduHub_backend.controller;
+
+import com.eduHub.eduHub_backend.component.StudentService;
+import com.eduHub.eduHub_backend.model.Course;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CoursesController {
+    public List<Course> courseList = new ArrayList<>();
+
+    // Constructor to initialize data
+    public CoursesController() {
+        courseList.add(new Course(1,"Java",2));
+        courseList.add(new Course(2,"C++",2));
+        courseList.add(new Course(3,"Python",2));
+        courseList.add(new Course(4,"DBMS",1));
+        courseList.add(new Course(5,"ML",1));
+    }
+
+    // Return one Course
+    @GetMapping("courses")
+    public ResponseEntity<Course> getCourses(){
+        Course course  = new Course(1,"Java",2);
+        return new ResponseEntity<>(course, HttpStatus.OK);
+    }
+
+    // Return list of Courses
+    @GetMapping("course-list")
+    public ResponseEntity<List<Course>> getCourseList(){
+               return new ResponseEntity<>(courseList,HttpStatus.OK);
+    }
+
+//    // Get using Id
+//    @GetMapping("courses/{courseCode}")
+//    public ResponseEntity<Course> CoursePathVariable(@PathVariable ("courseCode") int courseCode){
+//        Course course = new Course(courseCode);
+//        return new ResponseEntity<>(course,HttpStatus.OK);
+//    }
+
+    @GetMapping("course-query")
+    public ResponseEntity<Course> CourseResponseVariable(@RequestParam int courseCode,
+                                                                         @RequestParam String subjectName,
+                                                                         @RequestParam int courseCredits)
+    {
+        Course course = new Course(courseCode, subjectName,courseCredits);
+        return ResponseEntity.ok(course);
+    }
+
+    // Create a new Student
+    @PostMapping("create-course")
+    public ResponseEntity<Course> createCourse(@RequestBody Course course){
+        System.out.println(course.getCourseCode());
+        System.out.print(course.getSubjectName());
+        System.out.println(course.getCourseCredits());
+        return ResponseEntity.ok(course);
+    }
+
+    // Update based on id
+    @PutMapping("{courseCode}/update-course")
+    public ResponseEntity updateCourse(@PathVariable("courseCode") int courseCode,
+                                         @RequestBody Course course)
+    {
+        return ResponseEntity.accepted().body(course);
+    }
+
+    // Delete Student
+    @DeleteMapping("{courseCode}/delete-course")
+    public ResponseEntity deleteCourse(@PathVariable("courseCode") int courseCode){
+        return ResponseEntity.accepted().body("Data deleted successfully");
+    }
+
+}
