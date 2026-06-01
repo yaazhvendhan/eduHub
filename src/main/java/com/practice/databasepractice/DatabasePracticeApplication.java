498e159d37254781a188bc51991a91ab309b3478
yaazhvendhan <yaazhvendhanms@gmail.com>
2026-06-01 14:44:15 +0530

    initial commit

---
+++ src/main/java/com/practice/databasepractice/DatabasePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.databasepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabasePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/databasepractice/DatabasePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.databasepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
