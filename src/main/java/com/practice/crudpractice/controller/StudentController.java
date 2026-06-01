b41286afda52b58cb70df7a739cb21681a0e7168
yaazhvendhan <yaazhvendhanms@gmail.com>
2026-06-01 11:39:13 +0530

    feat: implement REST API GET method

--- src/main/java/com/practice/crudpractice/controller/StudentController.java
+++ src/main/java/com/practice/crudpractice/controller/StudentController.java
@@ -2,16 +2,24 @@ package com.practice.crudpractice.controller;
 
 import com.practice.crudpractice.model.Student;
 import com.practice.crudpractice.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class StudentController {
+    @Autowired
     StudentService studentService;
-    @GetMapping("students")
+    @GetMapping("/students")
     public List<Student> getAllStudent(){
                 return studentService.getAllStudent();
     }
+    @GetMapping("students/{roll_no}")
+    public Student getStudentByRollNo(@PathVariable("roll_no") int roll_no){  //@PathVariable --> Says that the variable is dynamic . ie tin he path students/roll_no the roll number can change
+        return studentService.getStudentByRollNo(roll_no);
+    }
+
 }
--- src/main/java/com/practice/crudpractice/service/StudentService.java
+++ src/main/java/com/practice/crudpractice/service/StudentService.java
@@ -12,7 +12,21 @@ public class StudentService {
             new Student(2,"Saturn","C++"),
             new Student(3,"Snow","Python")) ;
     public List<Student> getAllStudent() {
+        return studentList;
+    }
 
-        return null;
+    public Student getStudentByRollNo(int roll_number) {
+//        int index = 0;
+//        for(int i=0;i<studentList.size();i++){
+//            if(studentList.get(i).getRoll_no()==roll_number){
+//                index =i;
+//            }
+//        }
+        for(Student s : studentList){
+            if(s.getRoll_no()==roll_number){
+                return s;
+            }
+        }
+        return new Student(0,"Student Not Found","Not Found");
     }
 }
