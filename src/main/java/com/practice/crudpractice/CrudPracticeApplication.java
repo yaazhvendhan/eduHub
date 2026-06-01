dca1fe1af41a790b5e070f664666e24bdf9fabd4
yaazhvendhan <yaazhvendhanms@gmail.com>
2026-06-01 09:21:23 +0530

    initial commit

---
+++ src/main/java/com/practice/crudpractice/CrudPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.crudpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudPracticeApplication.class, args);
+    }
+
+}
